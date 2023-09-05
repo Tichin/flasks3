0dbbe746a90e8746540fe4a3c3752f44a074e43b
Igor Oganesian <igor.oganesian@gmail.com>
2023-09-05 19:32:30 -0400

    uploads working

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import boto3
 from botocore.exceptions import ClientError
 # import uuid
 
-from flask import Flask, redirect, url_for, request, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
 
-from models import db, connect_db, File
+from models import db, connect_db
 
 app = Flask(__name__)
 
@@ -22,39 +22,40 @@ connect_db(app)
 @app.get("/")
 def home():
     """test home"""
+    def upload_file(file_name, bucket, object_name=None):
+        """Upload a file to an S3 bucket
+
+        :param file_name: File to upload
+        :param bucket: Bucket to upload to
+        :param object_name: S3 object name. If not specified then file_name is used
+        :return: True if file was uploaded, else False
+        """
+
+    # If S3 object_name was not specified, use file_name
+        if object_name is None:
+            object_name = os.path.basename(file_name)
+
+    # Upload the file
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(file_name, bucket, object_name)
+            print("RESPONSE: ", response)
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
+
+
+    s3 = boto3.client('s3')
+    # with open("testupload.txt", "rb") as f:
+    #     print("F: ", f)
+    #     s3.upload_fileobj(f, "flasks3-test", "files/")
+
+    s3.upload_file("testupload.txt", 'flasks3-test', "testuploads/testupload3")
 
     return render_template("index.html")
 
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-# If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-        print(response)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
 
 
-s3 = boto3.client('s3')
-
-# with open("testupload.txt", "rb") as f:
-#     s3.upload_fileobj(f, "flasks3-test", "files")
-
-s3.upload_file(
-    "testupload.txt", 'flasks3-test', 'files'
-)
+# print(testupload.txt)
